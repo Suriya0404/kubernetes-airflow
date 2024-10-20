0b4970e33d73eeee9115a7a0c0b353adbc7a01ea
suriyamohan <suriya.p.mohan@gmail.com>
2024-10-19 23:50:40 -0700

    initial commit

---
+++ dags/hello.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'Suriya',
+    'start_date': datetime(2024, 10,19),
+    'catchup': False
+}
+
+dag = DAG('hello_world',
+          default_args=default_args,
+          schedule=timedelta(days=1))
+
+t1 = BashOperator(task_id="hello",
+                  bash_command='echo "Hello World!"',
+                  dag=dag)
+
+t2 = BashOperator(task_id="hello",
+                  bash_command='echo "Hello World!"',
+                  dag=dag)
+
+t1 >> t2
